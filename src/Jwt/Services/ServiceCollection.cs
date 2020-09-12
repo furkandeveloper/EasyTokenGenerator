69bac9370b9d7147b319573a98556d3cf3488d13
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-12 22:17:38 +0300

    Add service collection extension method for startup.cs

---
+++ src/Jwt/Services/ServiceCollection.cs
@@ -0,0 +1,47 @@
+﻿using Jwt.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Jwt.Services
+{
+    /// <summary>
+    /// This class includes options for Jwt Token Generator.
+    /// </summary>
+    public static class ServiceCollection
+    {
+        /// <summary>
+        /// Extensions method for service collection object in startup.cs
+        /// </summary>
+        /// <param name="services">
+        /// Service collection object
+        /// </param>
+        /// <param name="options">
+        /// Default JwtBearerOptions object
+        /// </param>
+        /// <returns>
+        /// Service collection
+        /// </returns>
+        public static IServiceCollection AddEasyJwtToken(this IServiceCollection services,Action<JwtOptions> options)
+        {
+            JwtOptions jwtOptions = new JwtOptions();
+            options.Invoke(jwtOptions);
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(opt =>
+                {
+                    opt = jwtOptions;
+                    opt.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecurityKey));
+                });
+            services.AddSingleton(jwtOptions);
+            return services;
+        }
+    }
+}
