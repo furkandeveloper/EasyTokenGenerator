a79235213013d76d4b13eb31ee55b65e7ca97394
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-12 22:14:33 +0300

    Add options class for jwt bearer options

---
+++ src/Jwt/Models/JwtOptions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jwt.Models
+{
+    /// <summary>
+    /// This class includes jwt bearer options.
+    /// </summary>
+    public class JwtOptions : JwtBearerOptions
+    {
+        public string SecurityKey { get; set; } = "mySecurityKey";
+    }
+}
