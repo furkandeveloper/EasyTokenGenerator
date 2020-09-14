7f45c799c4857e376a0b407240ee1035fce1770b
Furkan GÜNGÖR <furkan.dvlp@hotmail.com>
2020-09-14 22:42:06 +0300

    Add Request dto for generate jwt token

---
+++ samples/JwtSample/Dtos/GenerateJwtTokenRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtSample.Dtos
+{
+    public class GenerateJwtTokenRequestDto
+    {
+        public string Email { get; set; } = "string@string.com";
+
+        public string Password { get; set; } = "123456";
+    }
+}
